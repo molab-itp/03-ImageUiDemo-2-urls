8e032911befb38cc762e97e515bb3530027182ab
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-09-14 16:24:30 -0400

    ImageUiDemo-urls

---
+++ ImageUiDemo-urls/ImageUiDemo-urls/ImageUiDemo_urlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageUiDemo_urlsApp.swift
+//  ImageUiDemo-urls
+//
+//  Created by jht2 on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageUiDemo_urlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Page1()
+        }
+    }
+}
